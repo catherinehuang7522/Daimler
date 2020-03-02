5c14c8d7170699193cf2c3fd219eac41c4068286
willebolin <44989047+willebolin@users.noreply.github.com>
2020-03-01 23:29:42 -0800

    Update index.js

--- CabanaTrivia/pages/index.js
+++ CabanaTrivia/pages/index.js
@@ -15,14 +15,21 @@ const styles = ({
 export default class IndexPage extends Component {
   constructor(props) {
     super(props)
-    this.state = { startGame: true }
+
+    this.state = { 
+      startGame: true 
+    };
 
     this.onClickStartGame = this.onClickStartGame.bind(this)
   }
 
   onClickStartGame() {
     //change the state when the user clicks start game
-    this.setState({startGame: false})
+    if(this.state.startGame === true){
+      this.setState({startGame: false})
+    }else{
+      this.setState({startGame: true})
+    }
   }
   render() {
 
