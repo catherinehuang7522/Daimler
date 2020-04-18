2d47df4d5ec3a17469f93f58d2c2fa2787ce2590
Wilhelm Bolin <44989047+willebolin@users.noreply.github.com>
2020-04-18 00:45:50 -0700

    merge from master to master-dev

---
+++ src/components/CategoriesButton.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import { styles } from "../stylesheet.js";
+
+//Recycle Category Button, by default the button is unselected
+class CategoriesButton extends Component {
+  render() {
+    return (
+      <Button
+        style={styles.unselectedButton}
+        variant="contained"
+        color="secondary"
+        onClick={this.props.onClick}
+      >
+        {this.props.text}
+      </Button>
+    );
+  }
+}
+export default CategoriesButton;
