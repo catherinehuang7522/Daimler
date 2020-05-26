e266bcdf2e610870c7425487b520364e27b1e413
Wilhelm Bolin <44989047+willebolin@users.noreply.github.com>
2020-05-26 00:05:36 -0700

    merge

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,12 +1,15 @@
 import { styles } from "../stylesheet";
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 
 
 class LandingPage extends Component {
   render() {
     return (
       <div style={styles.root}>
+        <Grid style={styles.title}> Cabana Trivia  </Grid>
+        <p>PUT SOME INFO TEXT HERE </p>
         <Button
           style={styles.answerButton}
           onClick={() => this.props.callback("PROFILES")}
